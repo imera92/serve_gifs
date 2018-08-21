d67f8e4da84a22a77c673ac319ab1dcf889ad8c1
Ivan Mera <imera92@gmail.com>
2018-08-21 15:39:19 -0500

    Fixes

--- client.py
+++ client.py
@@ -3,6 +3,7 @@ import glob
 sys.path.append('gen-py')
 sys.path.insert(0, glob.glob('../thrift-0.11.0/lib/py/build/lib*')[0])
 from topgifs import TopGifsService
+from src.Gif import Gif
 from thrift import Thrift
 from thrift.transport import TSocket
 from thrift.transport import TTransport
--- server.py
+++ server.py
@@ -3,8 +3,8 @@ import sys
 sys.path.append("gen-py")
 sys.path.insert(0, glob.glob('../thrift-0.11.0/lib/py/build/lib*')[0])
 from topgifs import TopGifsService
-from src import DbInterface
-from src import Gif
+from src.DbInterface import DbInterface
+from src.Gif import Gif
 from thrift.transport import TSocket
 from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
@@ -20,7 +20,7 @@ class FetchGifHandler:
         print("Consulted Gif ID: " + gif_id)
         dbi.getGifById("1")
         print("[Server] Creating Gif instance")
-        gif = new Gif(dbi.result)
+        gif = Gif(dbi.result)
         print("[Server] Answering request")
         return gif
 	
