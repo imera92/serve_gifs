48d47f5e829c7aadbd78f4b11ac04ff278aa8d12
Ivan Mera <imera92@gmail.com>
2018-08-21 12:22:46 -0500

    Cambio en retorno de dbinterface:

--- src/DbInterface.py
+++ src/DbInterface.py
@@ -7,18 +7,19 @@ class DbInterface:
 	db="PROYECTO"
 	charset="utf8mb4"
 	connection=None
+	result=None
 
 	def openConnection(self):
 		# Connect to the database
 		self.connection = pymysql.connect(self.host,user='root',password='root',db='PROYECTO',charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
 
 	def getGifById(self, gif_id):
-		self.openConnection()		
 		try:
+			self.openConnection()
 			with self.connection.cursor() as cursor:
 				sql = "SELECT * FROM `gifs_gif_old` WHERE `id`=%s"
 				cursor.execute(sql, (gif_id))
-				result = cursor.fetchone()
-				return result	
+				self.result = cursor.fetchone()
 		finally:
 			self.connection.close()
+			return self.result
